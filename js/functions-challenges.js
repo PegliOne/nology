73fc51043c5a6ef0183eb5bb0c6d33ea7b01e7b3
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-08-30 10:19:57 +1000

    Completes js functions challenge

---
+++ js/functions-challenges.js
@@ -0,0 +1,43 @@
+// Challenge: Calculating Moon Orbits
+
+function calculateMoonOrbits(amountOfEarthDays) {
+  return amountOfEarthDays / 27;
+}
+
+// Challenge: Working with Circles
+
+function calculateAreaOfCircle(radius) {
+  return (radius ** 2 * Math.PI).toFixed(2);
+}
+
+function calculateAreaOfCircle(radius) {
+  return (radius * 2 * Math.PI).toFixed(2);
+}
+
+// Challenge: Years to Days & Seconds
+
+function calculateAgeInDays(ageInYears) {
+  return ageInYears * 365;
+}
+
+function calculateAgeInSeconds(ageInYears) {
+  return ageInYears * 365 * 24 * 60 * 60;
+}
+
+// Challenge: Return the Remainder from Two Numbers
+
+function calculateRemainder(num1, num2) {
+  return num1 % num2;
+}
+
+// Challenge: Basketball Points
+
+function calculateRemainder(amountOfTwoPointers, amountOfThreePointers) {
+  return amountOfTwoPointers * 2 + amountOfThreePointers * 3;
+}
+
+// Challenge: Less Than 100?
+
+function lessThan100(num1, num2) {
+  return num1 + num2 < 100;
+}
