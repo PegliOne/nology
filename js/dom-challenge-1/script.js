cf2385d29a71b93f3c7efdaa4bfe56fd70281eb5
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-09-07 16:47:52 +1000

    Complete form input functionality

--- js/dom-challenge-1/script.js
+++ js/dom-challenge-1/script.js
@@ -1,36 +1,57 @@
 // DOM Selectors
 
-const submitButton = document.querySelector('.button');
-const beerImage = document.querySelector('img');
-const messageDiv = document.querySelector('#message');
-console.log(message);
+const beerImage = document.querySelector("img");
+const form = document.querySelector("form");
+const ageInput = document.querySelector("#age");
+const countryInput = document.querySelector("#country");
+const drunkInput = document.querySelector("#drunk");
+const submitButton = document.querySelector("#btn");
+const messageDiv = document.querySelector("#message");
 
-// Business Logic
+// DOM Logic
 
-const person1 = {
-  age: 18,
-  legalDrinkingAge: 18,
-  isDrunk: false  
-}
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  getFormData();
+});
+
+const getFormData = () => {
+  const data = new FormData(document.querySelector("form"));
+  const age = data.get("age");
+  const country = data.get("country");
+  const drunkennessLevel = data.get("drunk");
+  if (age && country && drunkennessLevel) {
+    const person = {
+      age: age,
+      legalDrinkingAge: country === "USA" ? 21 : 18,
+      isDrunk: drunkennessLevel > 3,
+    };
+    renderEntryState(person);
+  }
+};
 
 const renderEntryState = (person) => {
   const message = checkIfCanEnterPub(person);
   if (message === "Come in") {
+    submitButton.classList.remove("cannot-enter");
     submitButton.classList.add("can-enter");
     beerImage.setAttribute("src", "./assets/green_beer.svg");
   } else {
+    submitButton.classList.remove("can-enter");
     submitButton.classList.add("cannot-enter");
     beerImage.setAttribute("src", "./assets/red_beer.svg");
-  }   
+  }
   messageDiv.textContent = message;
-}
+};
+
+// Business Logic
 
 const checkIfCanEnterPub = (person) => {
   if (!person.isDrunk && person.age >= person.legalDrinkingAge) {
     return "Come in";
+  } else if (person.isDrunk) {
+    return "You can't come in for you're drunk";
   } else {
-    return "You can't come in";
-  }     
-}
-
-renderEntryState(person1);
+    return "You can't come in for you're too young given your country's legal drinking age";
+  }
+};
