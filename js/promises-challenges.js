68f6fcb231222fa766339db91a7ad59e7328854f
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-09-13 11:59:27 +1000

    Fixed filter conditions

--- js/promises-challenges.js
+++ js/promises-challenges.js
@@ -17,15 +17,15 @@ const delayedIncDec = (n, mode, timeout = 2500) => {
 }; 
 
 delayedIncDec(2, "inc", 1000);
-delayedIncDec(4, "invalid", 3000);
+delayedIncDec(4, "dec", 3000);
 delayedIncDec(3, "inc", 2000);
 
 // Challenge: Delayed Filter
 
 const delayedFilter = (array, callback, timeout = 2500) => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (!Array.isArray(array) || typeof callback !== "function") {
+    setTimeout(() => {   
+      if (!(Array.isArray(array) && typeof callback === "function")) {
         reject(new Error("Error: Invalid array or function"));
       }  
       resolve(array.filter(callback));
@@ -35,3 +35,4 @@ const delayedFilter = (array, callback, timeout = 2500) => {
 
 delayedFilter([1, 2, 3, 4], (num) => num % 2 === 0, 1000);
 delayedFilter(["string", 5, NaN, 6], (item) => typeof item === "number" && !isNaN(item), 2000);
+delayedFilter("string", (num) => num % 2 === 0);
