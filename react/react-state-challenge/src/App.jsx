fd24ec10c8fb69f0ad5ecf865ff4ce94b98484ea
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-09-27 17:34:55 +1000

    Completes react state challenge

--- react/react-state-challenge/src/App.jsx
+++ react/react-state-challenge/src/App.jsx
@@ -2,11 +2,7 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
-  return (
-    <main>
-      <Card />
-    </main>
-  );
+  return <Card />;
 }
 
 export default App;
--- react/react-state-challenge/src/components/Card.jsx
+++ react/react-state-challenge/src/components/Card.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
+import styles from "./Card.module.scss";
 
 const Card = () => {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(6);
+  const [isDay, setIsDay] = useState(true);
 
   const generateRandomTime = () => {
-    const hours = 3;
-    const randomTime = hours + ":00";
-    setTime(randomTime);
+    const time = Math.floor(Math.random() * 24);
+    setTime(time);
+    const isDay = time >= 6 && time < 18;
+    setIsDay(isDay);
   };
 
+  let cardClasses = styles.card;
+  let contentClasses = styles.content;
+
+  if (!isDay) {
+    cardClasses += ` ${styles.night}`;
+    contentClasses += ` ${styles.night}`;
+  }
+
   return (
-    <section>
-      <button onClick={generateRandomTime}>Click for Random Time</button>
-      <p>Time: {time}</p>
-    </section>
+    <main className={cardClasses}>
+      <section className={contentClasses}>
+        <h1>Day / Night Mode App</h1>
+        {isDay ? (
+          <img src="sun.svg" alt="Sun Icon" />
+        ) : (
+          <img src="moon.svg" alt="Moon Icon" />
+        )}
+        <button onClick={generateRandomTime}>Click for Random Time</button>
+        <p>Time: {time}:00</p>
+      </section>
+    </main>
   );
 };
 
